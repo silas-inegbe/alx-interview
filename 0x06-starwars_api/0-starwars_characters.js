04fea31a58a5add373a08354b164620e01d1dae2
silas-inegbe <kingssilas18@gmail.com>
2023-09-13 23:31:13 +0000

    test

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,43 +2,30 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
 }
 
-const movieId = process.argv[2];
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
+request(movieEndpoint, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
 
-  if (response.statusCode !== 200) {
-    console.error('Failed to retrieve data. Status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  try {
-    const filmData = JSON.parse(body);
-    const characters = filmData.characters;
-
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error('Error:', charError);
-        } else {
-          const characterData = JSON.parse(charBody);
-          console.log(characterData.name);
-        }
-      });
-    });
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
-    process.exit(1);
+    sendRequest(characterList, 0);
   }
 });
-
